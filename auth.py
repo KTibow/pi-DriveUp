85623e665efd966e1a6a54815258462b04711447
kendell <KTibow@users.noreply.github.com>
2020-05-24 12:59:07 -0700

    Update auth.py

--- auth.py
+++ auth.py
@@ -15,7 +15,7 @@ def requireit(B):
 requireit([["onedrivesdk", "git+https://github.com/OneDrive/onedrive-sdk-python.git"]])
 if onedrivesdk == 0:
 	print("Package error... If there's an exception later, replace this and everything above with \"import onedrivesdk\". Make sure you've installed it with \"pip install git+https://github.com/OneDrive/onedrive-sdk-python.git\" too.")
-import os
+import os, urllib.request
 from onedrivesdk.helpers import GetAuthCodeServer
 from time import sleep
 
@@ -23,15 +23,15 @@ redirect_uri = 'http://localhost:8080/'
 print("What's your client secret? Make sure you didn't copy anything else.")
 client_secret = input("Paste it in and press enter: ")
 scopes = ['wl.signin', 'wl.offline_access', 'onedrive.readwrite']
-
+client_id=input("What's your client ID? Make sure you didn't copy anything else. Paste it in and press enter: ")
 client = onedrivesdk.get_default_client(
-    client_id=input("What's your client ID? Make sure you didn't copy anything else. Paste it in and press enter: "), scopes=scopes)
+    client_id=client_id, scopes=scopes)
 
 auth_url = client.auth_provider.get_auth_url(redirect_uri)
 
-print("A blank webpage will pop up in 10 seconds. If it says that it couldn't connect, then re-run this script.")
+print("A blank webpage will pop up in 7 seconds. If it says that it couldn't connect, then re-run this script.")
 print("It will automatically close itself.")
-sleep(10)
+sleep(7)
 print("If a window doesn't pop up in the next few seconds, or if")
 print("this machine doesn't have an accesible web browser, copy and paste ")
 print(auth_url)
@@ -42,5 +42,12 @@ code = GetAuthCodeServer.get_auth_code(auth_url, redirect_uri)
 client.auth_provider.authenticate(code, redirect_uri, client_secret)
 
 client.auth_provider.save_session()
-os.system("sudo mv /home/pi/session.pickle /root/session.pickle")
+os.system("sudo mv "+os.getcwd()+"/session.pickle /root/session.pickle")
 print("Auth code set up. You only need to run this once, unless you start getting errors.")
+print("Downloading backup.py and moving it...")
+bckscript = str(urllib.request.urlopen("https://raw.githubusercontent.com/KTibow/pi-DriveUp/master/backup.py")).replace("your_client_id", client_id)
+bckfile = open("backup.py", "w")
+bckfile.write(bckscript)
+bckfile.close()
+os.system("sudo mv "+os.getcwd()+"/backup.py /root/backup.py")
+print("Done!")
